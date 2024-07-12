88852ab3eb5fb6bd6c3665c026c4cd21d1e090ec
Aadarsh Mane <onlyaddy68@gmail.com>
2024-07-13 02:31:28 +0530

    added mail

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running on  http://localhost:${port}/`);
 });
