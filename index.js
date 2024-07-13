c53ed7201fad5a6ceee416a214520f155b1c0e46
Aadarsh Mane <onlyaddy68@gmail.com>
2024-07-14 01:54:23 +0530

    update phone call mail feature

--- index.js
+++ index.js
@@ -45,6 +45,38 @@ app.post("/send-email", (req, res) => {
     res.send({ success: true, info });
   });
 });
+app.post("/send-phone-call", (req, res) => {
+  const { phone } = req.body;
+  const subject = "Phone Call Request";
+  const text = `Phone Number: ${phone}`;
+
+  sendMail("", "", subject, text, res);
+});
+
+// Helper function to send email
+function sendMail(name, email, subject, text, res) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "onlyaddy68@gmail.com",
+      pass: "qkto yaph mtui ssag",
+    },
+  });
+
+  const mailOptions = {
+    from: "onlyaddy68@gmail.com",
+    to: "20sdeveloper4209@gmail.com",
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send({ error: error.toString() });
+    }
+    res.send({ success: true, info });
+  });
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
