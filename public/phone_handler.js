2337e5f7375ce8f322f00bad16b54bb4f0229795
Aadarsh Mane <onlyaddy68@gmail.com>
2024-07-19 19:08:53 +0530

    merging

--- public/phone_handler.js
+++ public/phone_handler.js
@@ -49,3 +49,56 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", handleFormSubmit);
   });
 });
+// document.addEventListener("DOMContentLoaded", function () {
+//   function handleFormSubmit(event) {
+//     event.preventDefault();
+//     const form = event.target;
+//     const formData = new FormData(form);
+
+//     const data = {};
+//     formData.forEach((value, key) => {
+//       data[key] = value;
+//     });
+
+//     // Validate phone number before submitt
+//     const phoneNumber = data["phone"];
+//     if (phoneNumber.length !== 10) {
+//       showAlert("Please enter a valid 10-digit phone number.");
+//       return;
+//     }
+
+//     fetch("/send-phone-call", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log("Success:", data);
+//         showAlert("Phone call request submitted successfully!");
+//         form.reset();
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//         showAlert("Error submitting phone call request.");
+//       });
+//   }
+
+//   function showAlert(message) {
+//     const alertBox = document.getElementById("custom-alert");
+//     const alertMessage = document.getElementById("custom-alert-message");
+//     const alertClose = document.getElementById("custom-alert-close");
+
+//     alertMessage.textContent = message;
+//     alertBox.style.display = "block";
+
+//     alertClose.onclick = function () {
+//       alertBox.style.display = "none";
+//     };
+//   }
+
+//   const phoneForm = document.getElementById("phoneForm");
+//   phoneForm.addEventListener("submit", handleFormSubmit);
+// });
